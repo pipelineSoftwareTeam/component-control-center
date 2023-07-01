4cbc9bd08c2113ef2fe7698c109cc347cd8b9a80
1sAndZeros <rikie_p@hotmail.com>
2023-07-01 16:13:28 +0100

    add eslint prettier config

--- client/src/components/LandingImage.jsx
+++ client/src/components/LandingImage.jsx
@@ -1,7 +1,7 @@
 import landingImg from '../assets/images/landing-img.svg';
 
 function LandingImage() {
-	return <img src={landingImg} className='landing-img' alt='landing-img' />;
+  return <img src={landingImg} className='landing-img' alt='landing-img' />;
 }
 
 export default LandingImage;
